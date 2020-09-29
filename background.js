61bd852c35da24c8eead05069c4726aedc7e1dbb
0pc0dxcc <siddhusids34@gmail.com>
2020-09-29 23:40:02 +0530

    Working manifest.json file and added some style for the popup.

---
+++ background.js
@@ -0,0 +1,13 @@
+function getid() {
+    window.onload = function() {
+        .addEventListener("click", function() {
+            alert("hi");
+        });
+    }
+}
+
+/*
+ Add an event listener that runs when an event occurs.
+ element.addEventListener(event, function, useCapture);
+ element.addEventListener("click", function(){ myFunction(p1, p2); }); 
+ */
